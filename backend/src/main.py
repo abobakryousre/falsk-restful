7070c530b3fc9c0e3a45e7e94482335ed5df47e2
abobakr <abobakryousre@vodafone.com>
2023-03-24 16:42:10 +0200

    exercises put, delete methods

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_restful import Api
-from resources.exercises import Exercieses
+from resources.exercises import ExerciesesList, Exercieses
 PORT = 5000
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Exercieses,'/exercieses')
+api.add_resource(ExerciesesList,'/exercieses')
+api.add_resource(Exercieses,'/exercieses/<int:exerciese_id>')
 
 if __name__ == "__main__":
     app.run(debug=True,port=PORT)
--- backend/src/resources/exercises.py
+++ backend/src/resources/exercises.py
@@ -2,8 +2,21 @@ from flask_restful import Resource
 
 
 
-class Exercieses(Resource):
+class ExerciesesList(Resource):
     def get(self):
         return {"data": "empty resourece"}
     def post(self):
         return {"data": "exerciese created"}
+    
+
+class Exercieses(Resource):
+
+    def get(self,exerciese_id):
+
+        return {"data": f"return {exerciese_id}"}
+    
+    def put(self,exerciese_id):
+        return {"data": f"{exerciese_id} updated"}
+    
+    def delete(self,exerciese_id):
+        return {"data": f"{exerciese_id} deleted"}
